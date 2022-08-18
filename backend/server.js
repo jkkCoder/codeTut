4f125b66ce582a78cbd020b6ab064b4ccbafd04f
jkkCoder <jayeshk118@gmail.com>
2022-08-18 22:35:27 +0530

    changed server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import programRoute from "./routes/programRoute.js"
+import path from "path"
 
 const app = express()
 
@@ -9,6 +10,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use("/",programRoute)
+
+const __dirname = path.resolve()
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/build")))
+
+    app.get("*",(req,res) => res.sendFile(path.resolve(__dirname,"frontend","build","index.html")))
+}
 
 const PORT = 5000 || process.env.PORT
 
